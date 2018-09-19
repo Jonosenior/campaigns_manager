3dcbf2792610eb1fc2b24523b641ce470193a5fa
jonathan senior <jonosenior@gmail.com>
2018-09-19 15:24:10 +0200

    Add actions to campaigns controller

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -6,4 +6,33 @@ class CampaignsController < ApplicationController
   def edit
     @campaign = Campaign.find(params[:id])
   end
+
+  def new
+    @campaign = Campaign.new
+    @todo_lists = @campaign.todo_lists
+  end
+
+  def show
+    @campaign = Campaign.find(params[:id])
+  end
+
+  def create
+    @campaign = Campaign.new(campaign_params)
+    if @campaign.save
+      flash[:success] = "Campaign added!"
+      redirect_to @campaign
+    else
+      flash.now[:danger] = "Invalid information."
+      render 'new'
+    end
+  end
+
+  private
+
+  def campaign_params
+    params.require(:campaign).permit(:title, :tags, :estimated_duration,
+                                      todo_lists_attributes:[:id, :title, :_destroy])
+  end
+
+
 end
