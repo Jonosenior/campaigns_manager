4a4681fd30865fa43b410cbcf9f944a8eec98d55
jonathan senior <jonosenior@gmail.com>
2018-09-19 11:58:19 +0200

    Add two possible types of user: novice and expert

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
+  before_save :default_type
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+end
+
+class Expert < User
+end
+
+class Novice < User
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180919085712) do
+ActiveRecord::Schema.define(version: 20180919090512) do
 
   create_table "campaigns", force: :cascade do |t|
     t.string "title"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20180919085712) do
     t.string "username"
     t.string "profession"
     t.string "service"
+    t.string "type"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
