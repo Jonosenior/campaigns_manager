2e89f472e81d1dceb5df922d40017176e30ece2c
jonathan senior <jonosenior@gmail.com>
2018-09-19 14:07:12 +0200

    Add has_many assocs for TodoLists to User and Campaign

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,3 +1,4 @@
 class Campaign < ApplicationRecord
   belongs_to :expert
+  has_many :todo_lists
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :username, :type, presence: true
+  has_many :todo_lists
 end
 
 class Expert < User
