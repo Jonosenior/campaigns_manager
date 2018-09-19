37b78676c8dab3dfee4f7cf0444c2f1fc49f8482
jonathan senior <jonosenior@gmail.com>
2018-09-19 14:37:19 +0200

    Add routes for campaigns and todolists (nested)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'campaigns/index'
-
-  get 'campaigns/new'
-
   root to: 'campaigns#index'
+  resources :campaigns do
+    resources :todo_lists, except: :index
+  end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
