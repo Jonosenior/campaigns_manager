ecc27b3b34fb5ea37ea566afee872dbee411b83e
jonathan senior <jonosenior@gmail.com>
2018-09-20 10:37:19 +0200

    Add full user attributes to sign up form (not inc status)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password)}
-      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password)}
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :type, :profession, :service)}
+      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :email, :password, :current_password, :profession, :service)}
     end
 end
