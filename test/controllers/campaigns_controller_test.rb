e8aed8e9bba3236d6e4eca54522f87ef5645c5fb
jonathan senior <jonosenior@gmail.com>
2018-09-20 16:44:52 +0200

    Edit tests

--- test/controllers/campaigns_controller_test.rb
+++ test/controllers/campaigns_controller_test.rb
@@ -1,20 +1,12 @@
 require 'test_helper'
 
 class CampaignsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   def setup
     @expert = users(:expert)
     @novice = users(:novice)
     @campaign = campaigns(:marketing)
-  end
-
-  # test "should get index" do
-  #   get campaigns_index_url
-  #   assert_response :success
-  # end
-
-  test "should get new" do
-    get campaigns_new_url
-    assert_response :success
   end
 
 end
