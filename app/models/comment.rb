b2a95171138d360cb221fe3c692c59ce85da82df
jonathan senior <jonosenior@gmail.com>
2018-09-21 14:12:51 +0200

    Make comment model polymorphic

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,17 +1,4 @@
 class Comment < ApplicationRecord
-  belongs_to :campaign, optional: true
   belongs_to :user
-  belongs_to :todo_list, optional: true
-
-  validates :title, presence: true
-  validates :campaign_xor_todo_list
-
-  private
-
-  def campaign_xor_todo_list
-    # binding.pry
-    if campaign && todo_list || !campaign && !todo_list
-      errors.add(:parent, "must have one parent")
-    end
-  end
+  belongs_to :commentable, polymorphic: true
 end
