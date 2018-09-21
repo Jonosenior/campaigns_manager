057223bef395d88b2191234b0bdde822dbf1c957
jonathan senior <jonosenior@gmail.com>
2018-09-21 15:14:06 +0200

    Add tests, fixtures and model validations for polymorphic comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :commentable, polymorphic: true
+
+  validates :title, :commentable_type, :commentable_id, :user_id, presence: true
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -3,11 +3,8 @@ require 'test_helper'
 class CommentTest < ActiveSupport::TestCase
   def setup
     @campaign_comment = comments(:campaign)
-    @no_user_comment = comments(:no_user)
     @todo_list_comment = comments(:todo_list)
-    @parentless_comment = comments(:parentless)
-    @double_parent_comment = comments(:double_parent)
-
+    @parentless_comment = comments(:parentless)    
   end
 
   test 'campaign comment should be valid' do
@@ -15,7 +12,8 @@ class CommentTest < ActiveSupport::TestCase
   end
 
   test 'should require user' do
-    assert_not @no_user_comment.valid?
+    @campaign_comment.user = nil
+    assert_not @campaign_comment.valid?
   end
 
   test 'todo list comment should be valid' do
@@ -31,9 +29,9 @@ class CommentTest < ActiveSupport::TestCase
     assert_not @parentless_comment.valid?
   end
 
-  test 'should not have both campaign and todolist' do
-  #   binding.pry
-    assert_not @double_parent_comment.valid?
-  end
+  # test 'should not have both campaign and todolist' do
+  # #   binding.pry
+  #   assert_not @double_parent_comment.valid?
+  # end
 
 end
