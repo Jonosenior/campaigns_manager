68be2cc3676136f8f98814700495956a79a33320
jonathan senior <jonosenior@gmail.com>
2018-09-21 12:29:55 +0200

    Add comment model tests, fixtures and validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :campaign, optional: true
   belongs_to :user
   belongs_to :todo_list, optional: true
+
+  validates :title, presence: true
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @campaign_comment = comments(:campaign)
+    @no_user_comment = comments(:no_user)
+    @todo_list_comment = comments(:todo_list)
+    @parentless_comment = comments(:parentless)
+  end
+
+  test 'campaign comment should be valid' do
+    assert @campaign_comment.valid?
+  end
+
+  test 'should require user' do
+    assert_not @no_user_comment.valid?
+  end
+
+  test 'todo list comment should be valid' do
+    assert @todo_list_comment.valid?
+  end
+
+  test 'should require title' do
+    @campaign_comment.title = '  '
+    assert_not @campaign_comment.valid?
+  end
+
+  test 'should require either campaign or todo list' do
+    assert_not @parentless_comment.valid?
+  end
+
+  test 'should not have both campaign and todolist' do
+    assert_not @double_parent.valid?
+  end
+
 end
