78dc51bc3a8d3ff2f48c7a7ded69898d59daa2db
jonathan senior <jonosenior@gmail.com>
2018-09-25 14:53:26 +0200

    Un-nest the routes for comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,9 @@ Rails.application.routes.draw do
   get 'users/index'
   root to: 'campaigns#index'
   resources :campaigns do
-    resources :todo_lists, except: :index do
-      resources :comments, only: [:create, :new]
-    end
-    resources :comments, only: [:create, :new]
+    resources :todo_lists, except: :index
   end
+  resources :comments, only: [:create, :new]
   devise_for :users
   scope '/expert' do
     resources :novices, only: [:index, :edit, :update], controller: 'users', type: 'Novice'
