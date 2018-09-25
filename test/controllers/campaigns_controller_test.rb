9585855652a3079a00e152bc4eaf24e157c3e6c3
jonathan senior <jonosenior@gmail.com>
2018-09-25 23:11:51 +0200

    Test create campaign controller actions

--- test/controllers/campaigns_controller_test.rb
+++ test/controllers/campaigns_controller_test.rb
@@ -13,7 +13,22 @@ class CampaignsControllerTest < ActionDispatch::IntegrationTest
     assert_no_difference 'Campaign.count' do
       post campaigns_path, params: { campaign: { title: 'Marketing', tags: 'social, priority', estimated_duration: '3 months'}}
     end
-    assert_redirected_to new_user_session_url
+    assert_redirected_to root_path
   end
 
+  test 'should redirect create when user is novice' do
+    login_as @novice
+    assert_no_difference 'Campaign.count' do
+      post campaigns_path, params: { campaign: { title: 'Marketing', tags: 'social, priority', estimated_duration: '3 months'}}
+    end
+    assert_redirected_to root_path
+  end
+
+  test 'should allow create when user is expert' do
+    login_as @expert
+    assert_difference 'Campaign.count', 1 do
+      post campaigns_path, params: { campaign: { title: 'Marketing', tags: 'social, priority', estimated_duration: '3 months'}}
+    end
+    assert_redirected_to campaign_path(id: 344023403)
+  end
 end
