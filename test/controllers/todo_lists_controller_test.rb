7ed379341443954fd9583ec89f8de4d5ed54c809
jonathan senior <jonosenior@gmail.com>
2018-09-26 11:05:03 +0200

    Add test for show todolist when not logged in

--- test/controllers/todo_lists_controller_test.rb
+++ test/controllers/todo_lists_controller_test.rb
@@ -1,4 +1,22 @@
 require 'test_helper'
 
 class TodoListsControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @marketing_todos = todo_lists(:marketing_todos)
+    @marketing_campaign = campaigns(:marketing)
+  end
+
+  test 'should redirect create when not logged in' do
+    assert_no_difference 'TodoList.count' do
+      post new_campaign_todo_list_path, params: { todo_list: { title: 'Some things to work on', user: 'novice', campaign: 'marketing', campaign_id: 2 }}
+    end
+    assert_redirected_to root_path
+  end
+
+  test 'should show todo list when not logged in' do
+    get campaign_todo_list_path(@marketing_campaign, @marketing_todos)
+    assert_response :success
+  end
+
 end
